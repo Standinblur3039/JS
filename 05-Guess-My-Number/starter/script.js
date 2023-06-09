59900a43ba3c7d743473a777076999fd2eff42f3
staninblur3039 <saitharun3039@gmail.com>
2023-06-09 12:25:42 +0530

    S3:E75 - up to game implementation / functionality

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,13 +1,15 @@
 'use strict';
 /*
  TODO
-console.log(document.querySelector('.message').textContent);
+console.log(document.querySelector('.message').textContent);  // print message 
 
-document.querySelector('.message').textContent = 'Correct Number !';
+document.querySelector('.message').textContent = 'Correct Number !';   //  print Correct Number in console 
 
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 20;
+document.querySelector('.number').textContent = 13; // show number as 13 
+document.querySelector('.score').textContent = 20;  // show as 20 
 console.log((document.querySelector('.guess').value = 12));
+
+windows + . = emoji
 */
 
 let Score = 20;
@@ -19,20 +21,27 @@ document.querySelector('.check').addEventListener('click', function () {
   console.log(guess, typeof guess);
 
   if (!guess) {
-    document.querySelector('.message').textContent = 'No Number !';
+    document.querySelector('.message').textContent = '⛔ No Number !';
   } else if (guess === secretNumber) {
-    document.querySelector('.message').textContent = 'Correct Number !';
+    document.querySelector('.message').textContent = '✅ Correct Number !';
   } else if (guess > secretNumber) {
     if (Score > 1) {
-      document.querySelector('.message').textContent = 'Number is Greater !';
+      document.querySelector('.message').textContent = '📈 Number is Greater !';
       Score--;
-      console.log((document.querySelector('.score').textContent = Score));
+      document.querySelector('.score').textContent = Score;
     } else {
-      document.querySelector('.message').textContent = 'You Lost !';
+      document.querySelector('.message').textContent = ' ☠️ You Lost !';
+      console.log((document.querySelector('.score').textContent = 0));
     }
   } else if (guess < secretNumber) {
-    document.querySelector('.message').textContent = 'Number is Smaller !';
-    Score--;
-    console.log((document.querySelector('.score').textContent = Score));
+    if (Score > 1) {
+      document.querySelector('.message').textContent =
+        ' 📉 Number is Smaller !';
+      Score--;
+      document.querySelector('.score').textContent = Score;
+    } else {
+      document.querySelector('.message').textContent = ' ☠️ You Lost !';
+      console.log((document.querySelector('.score').textContent = 0));
+    }
   }
 });
