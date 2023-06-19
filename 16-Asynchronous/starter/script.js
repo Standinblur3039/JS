67eac5dc330349800ecdd8e25443cdb261e186f5
staninblur3039 <saitharun3039@gmail.com>
2023-06-19 12:02:55 +0530

    First Ajax call

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -10,32 +10,34 @@ const getCountryData = function (country) {
   request.send();
 
   request.addEventListener('load', function () {
-    const [data] = JSON.parse(this.responseText); // we can also use the request.responseText
+    const [data] = JSON.parse(this.responseText);
     console.log(data);
 
-    const languages = Object.values(data.languages);
-    const currencies = Object.values(data.currencies);
+    const name = data.name.common;
+    const flag = data.flags.svg;
+    const region = data.region;
+    const language = Object.values(data.languages)[0];
+    const currency = Object.values(data.currencies)[0].name;
+
     const html = `
   <article class="country">
- <img class="country__img" src= "${data.flags.svg}">
- <div class="country__data">
-   <h3 class="country__name"> ${data.name.official}</h3>
-   <h4 class="country__region">${data.region}</h4>
-   <p class="country__row"><span>👫</span>${(data.population / 1000000).toFixed(
-     1
-   )} million</p>         
-   <p class="country__row"><span>🗣️</span>${languages[0]}</p>
-   <p class="country__row"><span>💰</span>${currencies[0].name}</p>
- </div>
-</article>`;
-
+    <img class="country__img" src="${flag}" />
+    <div class="country__data">
+      <h3 class="country__name">${name}</h3>
+      <h4 class="country__region">${region}</h4>
+      <p class="country__row"><span>👫</span>${(
+        +data.population / 1000000
+      ).toFixed(1)} people</p>
+      <p class="country__row"><span>🗣️</span>${language}</p>
+      <p class="country__row"><span>💰</span>${currency}</p>
+    </div>
+  </article>
+  `;
     countriesContainer.insertAdjacentHTML('beforeend', html);
     countriesContainer.style.opacity = 1;
   });
 };
 
-getCountryData('japan');
-getCountryData('india');
+getCountryData('portugal');
 getCountryData('usa');
-getCountryData('korea');
-getCountryData('brazil');
+getCountryData('germany');
